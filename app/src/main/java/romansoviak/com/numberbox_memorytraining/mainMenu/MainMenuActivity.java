103e8e668d20fe87a767e1cc5d19fef753ab89d6
Unknown <soviakroman@ukr.net>
2018-05-25 00:26:43 +0300

    Create Menu screen

---
+++ app/src/main/java/romansoviak/com/numberbox_memorytraining/mainMenu/MainMenuActivity.java
@@ -0,0 +1,24 @@
+package romansoviak.com.numberbox_memorytraining.mainMenu;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import romansoviak.com.numberbox_memorytraining.R;
+
+/**
+ * Created by Soviak Roman on 24.05.2018.
+ */
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu_activity);
+    }
+}
+
+
+
